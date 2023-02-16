71e0c9834509e0180ab3a1eadb6bfa233f82dbb8
akshay772 <akshay772@gmail.com>
2023-02-17 01:30:25 +0530

    updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Angular course with typescript
 
 ## Start new angular app
 ```npx ng new <project-name> --no-strict```
+
+### Implemented following : 
+1. component communication, 
+2. ViewChild, 
+3. EventEmitter nested to pass down from child to parent to grand-parent
+4. @Input 
+5. @Output
+6. (click)=someFunction
--- shop-reciepe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shop-reciepe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 
 }
