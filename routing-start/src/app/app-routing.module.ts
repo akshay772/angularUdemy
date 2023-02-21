f6112531ed70b92d3dd8e2e8340fdc334c89ac22
akshay772 <akshay772@gmail.com>
2023-02-21 17:48:58 +0530

    added routes handlers, auth-guard + resolver services

---
+++ routing-start/src/app/app-routing.module.ts
@@ -0,0 +1,56 @@
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule, Router} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {UsersComponent} from "./users/users.component";
+import {UserComponent} from "./users/user/user.component";
+import {ServersComponent} from "./servers/servers.component";
+import {ServerComponent} from "./servers/server/server.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AuthGuard} from "./auth-guard.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent}, // localhost:4200/,
+  {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}, // localhost:4200/users/<user_id>,
+    ]}, // localhost:4200/users,
+
+  {
+    path: 'servers',
+    // canActivate:[AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+      {
+        path: ':id',
+        component: ServerComponent,
+        resolve: {server: ServerResolver}
+      }, // localhost:4200/server/<server_id>,
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard]
+      }, // localhost:4200/servers,
+    ]}, // localhost:4200/servers,
+  // {path: 'not-found', component: PageNotFoundComponent},
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: {message: 'Page not found!'}
+  },
+  {path: '**', redirectTo: '/not-found'}, // double asterisk is all routes not defined above
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, )
+    // RouterModule.forRoot(appRoutes, {useHash: true})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
